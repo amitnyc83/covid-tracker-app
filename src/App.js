6e7bb18982c596ac1d73ad3a458f5575662f4579
amitnyc83 <amit_nyc@live.com>
2020-05-19 16:52:16 -0400

    create components file stucture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components'
+
 
 class App extends React.Component {
   render(){
     return(
       <div>
-      <h1>App</h1>
+       <Cards />
+       <Chart />
+       <CountryPicker />
       </div>
     )
   }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Cards = () => {
+  return (
+    <h1>Cards</h1>
+  )
+}
+
+
+export default Cards;
