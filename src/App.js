42512bba360405090e6f75239c907c9c123ce4e6
amitnyc83 <amit_nyc@live.com>
2020-05-19 21:18:22 -0400

    able to fetch data from API and wrap App in flex container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-
 // import Cards from './components/Cards/Cards';
 // import Chart from './components/Chart/Chart';
 // import CountryPicker from './components/CountryPicker/CountryPicker';
-
-import { Cards, Chart, CountryPicker } from './components'
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './API';
 
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render(){
     return(
-      <div>
+      <div className={styles.container}>
        <Cards />
-       <Chart />
        <CountryPicker />
+       <Chart />
       </div>
     )
   }
