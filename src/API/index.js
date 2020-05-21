31e1bf724da7ddb54f134cf5d9719c72f4159da1
amitnyc83 <amit_nyc@live.com>
2020-05-21 01:57:49 -0400

    fetching countries from API using async/await function with useState/useEffect

--- src/API/index.js
+++ src/API/index.js
@@ -9,7 +9,7 @@ export const fetchData = async () => {
 
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
-
+    console.log(error);
   }
 }
 
@@ -24,6 +24,16 @@ export const fetchDailyData = async () => {
     }));
     return modifiedData;
   } catch (error){
-    
+    console.log(error);
+  }
+}
+
+export const countries = async () => {
+  try{
+    const response = await axios.get(`${url}/countries`);
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
   }
 }
