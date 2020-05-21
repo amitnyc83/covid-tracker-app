46174be25a521e68762f6eb02f7f11fd8335ca4d
amitnyc83 <amit_nyc@live.com>
2020-05-21 04:29:14 -0400

    displays a barchart when a country is selected

--- src/App.js
+++ src/App.js
@@ -19,15 +19,15 @@ class App extends React.Component {
   handleCountryChange = async(country) => {
     const fetchedData = await fetchData(country);
 
-    this.setState({ data: fetchData });
+    this.setState({ data: fetchedData, country: country });
   }
   render(){
-    const { data } = this.state;
+    const { data, country } = this.state;
     return(
       <div className={styles.container}>
        <Cards data={data}/>
        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-       <Chart />
+       <Chart data={data} country={country}/>
       </div>
     )
   }
