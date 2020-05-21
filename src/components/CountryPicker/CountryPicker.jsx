0c21f7a422d7c529e9189d729934c37f785869f5
amitnyc83 <amit_nyc@live.com>
2020-05-21 13:54:19 -0400

    removing yarn lock file since we are using nom for dependencies

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -23,7 +23,7 @@ const CountryPicker = ({ handleCountryChange }) => {
   return (
     <FormControl className={styles.formControl}>
       <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-        <option value='Global'>Global</option>
+        <option value="">Global</option>
       {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
       </NativeSelect>
     </FormControl>
